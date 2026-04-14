1b6b2e4e3a27ab260623de4920c00e29de773562
NicolasMurg <nicolasmuf@gmail.com>
2026-04-13 22:10:10 -0400

    modified calculator p1

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Logarithm base must be positive and not equal to 1")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be positive")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
